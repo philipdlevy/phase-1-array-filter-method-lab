6c3fb2c1d8405ef34936f664539d1de15f266f34
Philip Levy <philipdlevy@gmail.com>
2022-02-19 18:36:57 -0700

    commit tree cee509fabedde1d7d99acc7faa78fa63478ba037

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arrayOfNames, string) {
+    const filteredArray = arrayOfNames.filter(name => name.toLowerCase() === string.toLowerCase())
+    return filteredArray
+};
+
+function fuzzyMatch(array, string) {
+    const filter = array.filter(name => name.indexOf(string) === 0)
+    return filter
+};
+
+function matchName(arrayObj, string) {
+    const filter = arrayObj.filter(objName => objName.name === string) 
+    return filter;
+}
